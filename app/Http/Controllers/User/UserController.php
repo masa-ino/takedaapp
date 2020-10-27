b130daa15bc739abb1efecc39e3f26d630b1b0c5
HORIZONium <masayuki121110@icloud.com>
2020-10-27 14:22:01 +0900

    no_delete_reserve when change times

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -50,7 +50,7 @@ class UserController extends Controller
             }
             $reserve_list[$value] = $kamoku;
         }
-        if(empty($reserve_list)){
+        if (empty($reserve_list)) {
             $reserve_list = null;
         }
         return view('user/training_list', [
@@ -70,12 +70,22 @@ class UserController extends Controller
 
         $user_id = Auth::id();
         $subject_id = Subject::where('user_id', $user_id)->get();
+        $reserved_list = [];
+        $no_search_time_list = [];
         foreach ($subject_id as $id) {
             $times_delete = Time::where("subject_id", $id->id)->get();
             if ($times_delete != null) {
+                $reserved_time_list = [];
                 foreach ($times_delete as $time_delete) {
+                    if ($time_delete->is_reserved == true) {
+                        array_push($reserved_time_list, $time_delete->time_name);
+                        array_push($no_search_time_list, $time_delete->time_name);
+                    }
                     $time_delete->delete();
                 }
+            }
+            if (!empty($reserved_time_list)) {
+                $reserved_list[$id->name] = $reserved_time_list;
             }
         }
         $spare_time = Spare_time::where("user_id", $user_id);
@@ -83,15 +93,30 @@ class UserController extends Controller
         if ($spare_time != null) {
             $spare_time->delete();
         }
-
+        $no_search_time_list = array_unique($no_search_time_list);
         $subjects = DB::select("SELECT * FROM subjects WHERE user_id = '$user_id'");
-
         foreach ($subjects as $subject) {
             foreach ($request->times as $time) {
                 $user_time = new Time();
                 $user_time->subject_id = $subject->id;
                 $user_time->time_name = $time;
                 $user_time->is_reserved = false;
+                if (isset($reserved_list[$subject->name])) {
+                    foreach ($reserved_list[$subject->name] as $reserved_time) {
+                        if ($reserved_time === $user_time->time_name) {
+                            print("time_name:");
+                            print($user_time->time_name);
+                            $user_time->is_reserved = true;
+                        }
+                    }
+                }
+                foreach($no_search_time_list as $no_search_time_name){
+                    if($time === $no_search_time_name){
+                        $user_time->no_search = true;
+                    }
+                }
+
+
                 $user_time->timestamps = false;
                 $user_time->save();
             }
@@ -138,7 +163,7 @@ class UserController extends Controller
             }
             $reserve_list[$value] = $kamoku;
         }
-        if(empty($reserve_list)){
+        if (empty($reserve_list)) {
             $reserve_list = null;
         }
         return view('user/training_list', [
